a7f5900ec917068fae701a81065e1bd8a79ab2da
Gurjit Singh <gurjitsinghisere@gmail.com>
2024-11-17 13:37:39 +0530

    enivornment port

--- pupp.mjs
+++ pupp.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import puppeteer from 'puppeteer';
 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT
 
 app.get('/fetch-html', async (req, res) => {
     const url = req.query.url;
