fc79740d5c72d31ca3f55b04af4ba37ee645aac6
Gurjit Singh <gurjitsinghisere@gmail.com>
2024-11-17 15:08:10 +0530

    script add browser

--- pupp.mjs
+++ pupp.mjs
@@ -2,25 +2,23 @@ import express from 'express';
 import puppeteer from 'puppeteer';
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/fetch-html', async (req, res) => {
     const url = req.query.url;
 
     if (!url) {
-        // format url : http://localhost:3000/fetch-html?url=https://www.playbook.com/s/gurjit/personal?assetToken=TBnnfycsnH6j8wTXbg4XoEK1
         return res.status(400).json({ error: 'Please provide a valid URL as a query parameter' });
     }
 
     try {
         const browser = await puppeteer.launch({
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
         });
         const page = await browser.newPage();
-        await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 }); // 60 seconds
+        await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
 
-        // Extract the src from the mux-player element
         const videoSrc = await page.evaluate(() => {
             const muxPlayer = document.querySelector('mux-player');
             return muxPlayer ? muxPlayer.getAttribute('src') : null;
@@ -32,10 +30,9 @@ app.get('/fetch-html', async (req, res) => {
             return res.status(404).json({ error: 'Video source not found.' });
         }
 
-        // Redirect to the video file
         res.redirect(videoSrc);
     } catch (error) {
-        console.error('Error:', error.message); // Log the error message
+        console.error('Error:', error.message);
         res.status(500).json({ error: 'Failed to fetch the page content.', details: error.message });
     }
 });
